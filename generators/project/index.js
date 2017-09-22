a12a4cd27d4160ac36bec33436dbea6138003ad2
Salvatore Laisa <moebiusmania@gmail.com>
2017-09-22 16:35:13 +0200

    first commit

---
+++ generators/project/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Generator = require('yeoman-generator');
+const yosay = require('yosay');
+const chalk = require('chalk');
+
+module.exports = class extends Generator {
+
+  start(){
+    this.log(yosay(
+      'Welcome to the kickass ' + chalk.red('Moebiusmania PROJECT') + ' generator!'
+    ));
+  }
+
+  prompts(){
+    const prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'What would you like this project to be called?',
+      default: 'sample-project'
+    }];
+
+    return this.prompt(prompts).then((props) => {
+      this.props = props;
+      const date = new Date();
+      this.props.year = date.getFullYear();
+    });
+  }
+
+  create(){
+    const name = this.props.name;
+
+    this.fs.copyTpl(
+      this.templatePath('**/*'),
+      this.destinationPath(name),
+      this.props,
+      {},
+      {globOptions: {dot: true}}
+    );
+  }
+
+  install() {
+    const dir = `${process.cwd()}/${this.props.name}`;
+    process.chdir(dir);
+
+    this.installDependencies({
+      npm: false,
+      bower: false,
+      yarn: true
+    });
+  }
+
+};
